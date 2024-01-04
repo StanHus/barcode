c5f67d9e6deeb17627ffcb9ca22fa67f2d62bb83
Stan <86333618+StanHus@users.noreply.github.com>
2024-01-04 16:00:52 +0000

    basic setup

---
+++ barcode-scanner.js
@@ -0,0 +1,31 @@
+// import Quagga from "quagga"; // ES6
+// // const Quagga = require("quagga").default; // Common JS (important: default)
+
+document.addEventListener("DOMContentLoaded", function () {
+  Quagga.init(
+    {
+      inputStream: {
+        name: "Live",
+        type: "LiveStream",
+        target: document.querySelector("#interactive"),
+        constraints: {
+          facingMode: "environment",
+        },
+      },
+      decoder: {
+        readers: ["code_128_reader"],
+      },
+    },
+    function (err) {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      Quagga.start();
+    }
+  );
+
+  Quagga.onDetected(function (data) {
+    console.log(data.codeResult.code);
+  });
+});

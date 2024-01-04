2c451be81089d92d0d0d54854584212aadd2541b
Stan <86333618+StanHus@users.noreply.github.com>
2024-01-04 16:10:36 +0000

    added upc to reader

--- barcode-scanner.js
+++ barcode-scanner.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", function () {
         },
       },
       decoder: {
-        readers: ["code_128_reader", "upc_reader", "upc_e_reader"],
+        readers: ["upc_reader"],
       },
     },
     function (err) {
